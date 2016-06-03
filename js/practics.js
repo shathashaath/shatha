f1201e78174a538e3577cbad8b84b46035209a98
Shatha Shaath <shatha_shaath@hotmail.com>
2016-06-03 08:26:14 -0700

    add project review

---
+++ js/practics.js
@@ -0,0 +1,82 @@
+// write the function above in var = format
+var finalResult = function(number1, number2) {
+    return 0;
+};
+
+function finalResult(number1, number2) {
+    return 0;
+}
+
+
+
+
+// write 5 ways of calling this function and use the return value
+finalResult(8,5);
+finalResult("5","8");
+var x = 3;
+var y = 4;
+finalResult (x,y);
+// x + y = 7
+// y - x = 1
+finalResult(x+y,y-x);
+
+// use finalResult and store the returned value and write it console
+
+var result=finalResult(1,2); // we used var to store the final result
+console.log ("Hello World"); // we called cosole.log and passed result variable as parameter.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function doSomthing() {
+    return 1;
+}
+// write the function above in var = format
+var doSomthing = function() {
+    return 1;
+};
+
+// use doSomthing function and store returned value
+var doSomthing=
+// use the returned value to alert
+
+// use the returned value and log it to console
+
+
+function doSomthingTo(number1) {
+    return "result";
+}
+var doSomthingTo = function(number1) {
+    return "result";
+};
+
+function writeToConsole() {
+    console.log("finalResult");
+}
+var writeToConsole = function() {
+    console.log("finalResult");
+};
+
+
+// possibles cities (Hawaii, Las Vegas, Los Angeles)
+// question 1: What do you like the most (choices = beach, night life, historic)
+// question 2: how do you like to travel (choices = bus, train, car)
+function getCityName(question1Answer, question2Answer, question3Answer, question4Answer, question5Answer){
+  var cityName = "";
+
+  return cityName;
+}
